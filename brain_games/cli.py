ced423b1dd19570eac458adce0a37723656c5bd6
Ekaterina Konstantinova <feldsher2812@gmail.com>
2023-01-28 00:11:19 +0300

    4 step

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -1,13 +1,15 @@
 #!/usr/bin/env python3
-
 import prompt
 
+
 def welcome_user():
-  name = prompt.string('May I have your name? ')
-  print('Hello, s' + name)
+    name = prompt.string('May I have your name? ')
+    print('Hello, s' + name)
+
 
 def main():
     welcome_user()
+
 
 if __name__ == '__main__':
     main()
