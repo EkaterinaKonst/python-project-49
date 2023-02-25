031bf094c0a0296839ac840180a247c4cd987fd3
Ekaterina Konstantinova <feldsher2812@gmail.com>
2023-02-25 23:41:26 +0300

    after 2 checking

---
+++ brain_games/scripts/brain_even.py
@@ -0,0 +1,10 @@
+from brain_games.engine import run
+from brain_games.games.braineven import RULES, generate_question_answer
+
+
+def main():
+    run(RULES, generate_question_answer)
+
+
+if __name__ == '__main__':
+    main()
