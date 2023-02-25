141458fb348cc6157af8dae752f7eb4aeec871fc
Ekaterina Konstantinova <feldsher2812@gmail.com>
2023-02-26 00:10:42 +0300

    after 2 checking

--- brain_games/games/braincalc.py
+++ brain_games/games/braincalc.py
@@ -10,7 +10,7 @@ def generate_question_answer():
     num_1 = random.randint(1, 100)
     num_2 = random.randint(1, 100)
     operator = random.choice(operators)
-    question  = f'{num_1} {operator} {num_2}'
+    question = f'{num_1} {operator} {num_2}'
     result_expression = str(ne.evaluate(question))
     answer = result_expression
     return question, answer
