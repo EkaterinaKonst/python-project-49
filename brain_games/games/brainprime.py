99237f41cfcde02f6c0e7dbf289a45490577a078
Ekaterina Konstantinova <feldsher2812@gmail.com>
2023-02-25 23:53:27 +0300

    after 2 checking

--- brain_games/games/brainprime.py
+++ brain_games/games/brainprime.py
@@ -3,8 +3,9 @@ import random
 
 RULES = 'Answer "yes" if given number is prime. Otherwise answer "no".'
 
+
 def is_prime(x):
-    for i in range(2, (x//2)+1):
+    for i in range(2, (x // 2) + 1):
         if x % i == 0:
             return 'no'
     return 'yes'
@@ -12,6 +13,5 @@ def is_prime(x):
 
 def generate_question_answer():
     question = random.randint(1, 100)
-    count = 0
     answer = is_prime(question)
     return question, answer
